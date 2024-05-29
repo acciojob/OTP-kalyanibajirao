fe35246a18acffa19e0a56f51a16db13ec32b514
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-05-29 13:10:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//your JS code here. If required.
-const codeInputs = document.querySelectorAll('.code');
+function handleInput(input) {
+            const nextInput = input.nextElementSibling;
+            const prevInput = input.previousElementSibling;
 
-        codeInputs.forEach((input, index) => {
-            input.addEventListener('input', (e) => {
-                if (e.target.value.length === 1) {
-                    // Move focus to the next input
-                    if (index < codeInputs.length - 1) {
-                        codeInputs[index + 1].focus();
-                    }
-                } else if (e.target.value.length === 0) {
-                    // Move focus to the previous input on backspace
-                    if (index > 0) {
-                        codeInputs[index - 1].focus();
-                    }
+            if (input.value.length === 1) {
+                if (nextInput) {
+                    nextInput.focus();
                 }
-            });
-        });
+            } else if (input.value.length === 0) {
+                if (prevInput) {
+                    prevInput.focus();
+                }
+            }
+        }
