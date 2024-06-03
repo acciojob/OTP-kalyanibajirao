101acaf138ae62072fc364953206a263cee1d1df
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-06-03 12:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function handleInput(input) {
-            const nextInput = input.nextElementSibling;
-            const prevInput = input.previousElementSibling;
+document.addEventListener('DOMContentLoaded', (event) => {
+  var inputs = Array.from(document.querySelectorAll('.code'));
 
-            if (input.value.length === 1) {
-                if (nextInput) {
-                    nextInput.focus();
-                }
-            } else if (input.value.length === 0) {
-                if (prevInput) {
-                    prevInput.focus();
-                }
-            }
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+      if (input.value.length > 0) {
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
         }
+      }
+    });
+
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Backspace' && input.value.length === 0) {
+        if (index > 0) {
+          inputs[index - 1].focus();
+        }
+      }
+    });
+  });
+
+  // Set the focus on the first input field when the page loads
+  if (inputs.length > 0) {
+    inputs[0].focus();
+  }
+});
